2813cb6488fe84d3f68f7c80e0443801032f1afc
Est16 <menenita_neny_@hotmail.com>
2018-02-01 20:54:27 +0100

    Pruebas_Reactor Parpadeo

---
+++ Pruebas_Reactor/Parpadeo/parpadeo.c
@@ -0,0 +1,13 @@
+#include <reactor/reactor.h>
+#include <reactor/blink_handler.h>
+#include <wiringPi.h>
+#include <stdio.h>
+
+int main ()
+{
+	wiringPiSetupGpio();
+	reactor* r= reactor_new();
+	reactor_add(r, (event_handler*) blink_handler_new(18, 200, 5));
+	reactor_run(r);
+	
+}
