bb03f734b6b78fe4a5a96be46a542dbb9f4068f0
Est16 <menenita_neny_@hotmail.com>
2018-02-16 14:19:24 +0100

    Borrado de Raton_Teclado

--- Pruebas_Hidraw/prueba.c
+++ Pruebas_Hidraw/prueba.c
@@ -9,17 +9,20 @@ int main() {
 	int teclado = open("/dev/hidraw0", O_RDONLY);
 	int raton = open("/dev/hidraw1", O_RDONLY);
 
-	fd_set fds;
-	FD_ZERO(&fds);
-	FD_SET(teclado, &fds);
-	FD_SET(raton, &fds);
+	fd_set *fds1;
+	fd_set fds2;
+	fds1=&fds2;
+	FD_ZERO(fds1);
+	FD_ZERO(&fds2);
+	FD_SET(teclado, fds1);
+	FD_SET(raton, &fds2);
 
 if (teclado<0)
 	printf("Error al abrir los archivos\n");
 	
 
 	for(;;) {
-		fd_set copia = fds;
+		fd_set copia = fds2;
 		int n = select(raton+1, &copia, NULL, NULL, NULL);
 		
 		int fd = -1;
--- Raton/raton.c
+++ Raton/raton.c
@@ -3,6 +3,53 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/select.h>
+
+
+static void mouse (event_handler* ev);
+
+event_handler* mouse_handler_new(int fd) {
+	return event_handler_new(fd, mouse);
+}
+
+static void mouse (event_handler* ev) {
+	printf("ev->fd %d\n",ev->fd);
+	char buf[1024];
+	int p= read (ev->fd, buf, 1024);
+	printf("bytes: %d\n", p);
+	if (p <0)
+		reactor_quit(ev->r);
+		
+	for(int i=0; i<p; ++i) {
+		printf("%x", buf[i]);
+	}
+	puts("");
+}
+
+int main () {
+	
+	int fd1 = open("/dev/hidraw1", O_RDONLY);
+		printf("fd1 %d\n",fd1);
+	
+	void* state= console_set_raw_mode(0);
+	reactor *r= reactor_new();
+	reactor_add(r, mouse_handler_new(fd1));
+	reactor_run(r);
+	console_restore(0,  state);
+	return 0;
+	
+}
+
+
+/*
+ * #include <reactor/reactor.h>
+#include <reactor/console.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
 
 
 #include <sys/types.h>
@@ -17,11 +64,13 @@ static void mouse (event_handler* ev);
 
 
 event_handler* keyboard_handler_new() {
+		printf("6\n");
 	return event_handler_new(0, keyboard);
 }
 
 static void keyboard (event_handler* ev) {
 	char buf[1];
+		printf("ev->fd %d\n",ev->fd);
 	if (read (ev->fd, buf, 1) <0 || buf[0]=='q')
 		reactor_quit(ev->r);
 	printf("pulsando %c\n", buf[0]);
@@ -34,8 +83,10 @@ event_handler* mouse_handler_new(int fd) {
 }
 
 static void mouse (event_handler* ev) {
+	printf("ev->fd %d\n",ev->fd);
 	char buf[1024];
 	int p= read (ev->fd, buf, 1024);
+	printf("bytes: %d\n", p);
 	if (p <0)
 		reactor_quit(ev->r);
 		
@@ -48,28 +99,32 @@ static void mouse (event_handler* ev) {
 
 int main () {
 	
-	/*
+	
 	int teclado = open("/dev/hidraw1", O_RDONLY); // no es cero
 	int raton = open("/dev/hidraw0", O_RDONLY);
 
 	printf("teclado %d\n", teclado);
 	printf("raton %d\n", raton);
-	* 
-	* */
 	
-		int fd1 = open("/dev/hidraw1", O_RDONLY);
-		//printf("fd1 %d\n",fd1);
+		int fd1 = open("/dev/hidraw0", O_RDONLY);
+		printf("fd1 %d\n",fd1);
 		
 		//int fd1=3; //no funciona!
 	
 	void* state= console_set_raw_mode(0);
 	reactor *r1= reactor_new();
-	reactor_add(r1, keyboard_handler_new());
+		printf("1\n");
 	reactor *r2= reactor_new();
+		printf("2\n");
+	reactor_add(r1, keyboard_handler_new());
+		printf("3\n");
 	reactor_add(r2, mouse_handler_new(fd1));
+		printf("4\n");
 	reactor_run(r1,r2);
+		printf("5\n");
 	//reactor_run(r2);
 	console_restore(0,  state);
 	return 0;
 	
 }
+*/
