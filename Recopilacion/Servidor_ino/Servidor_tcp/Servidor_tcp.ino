5c9f895b1396d13f65b950a66f234b13a4b1f362
Est16 <menenita_neny_@hotmail.com>
2018-06-12 23:14:26 +0200

    Se añade las funciones de simulacion de teclas en el servidor TCP.

--- Recopilacion/Servidor_ino/Servidor_tcp/Servidor_tcp.ino
+++ Recopilacion/Servidor_ino/Servidor_tcp/Servidor_tcp.ino
@@ -1,210 +1,147 @@
 #include <SPI.h>
 #include <Ethernet.h>
+//#include <Keyboard.h>
+//#include <Mouse.h>
 
 byte MiMac[]= {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
-byte MiIp[]= {192,168,1,38};
+byte MiIp[]= {192,168,1,45};
 
 EthernetServer ArduinoTCP = EthernetServer (1000);
 
 void setup() {
 
 Serial.begin(9600);
+//pinMode(10,OUTUPUT)
 Serial.println("Initializing ethernet connection");
 Ethernet.begin(MiMac, MiIp);
 ArduinoTCP.begin();
-
-
-
+// Keyboard.begin();  
 }
 
-int cont=0;
+
+char buf[8]={0,0,0,0,0,0,0,0};
+
+#define sensitivity 200
+
+typedef struct eventos {
+    byte x_plus[7]= {'3', '0', '1', '0', '0', '0', '0'};
+    byte x_less[7]= {'3', '0', '1', '5', '0', '0', '0'}; //    byte x_less[7]= {3, 0, -1, 15, 0, 0, 0};
+    byte y_plus[7]= {'3', '0', '0', '6', '1', '0', '0'}; //    byte y_plus[7]= {3, 0, 0, -16, -1, 0, 0};
+    byte y_less[7]= {'3', '0', '0', '6', '0', '0', '0'}; //    byte y_less[7]= {3, 0, 0, 16, 0, 0, 0};
+    byte ckd[7]= {'3', '2', '0', '0', '0', '0', '0'};
+    byte cki[7]= {'3', '1', '0', '0', '0', '0', '0'};
+};
+
+//raton* bytes;
 
 void loop() {
-  //ArduinoTCP.
+    eventos raton;
+    EthernetClient cliente = ArduinoTCP.available();
+    
+    //char c;
+    int i,p;
+    int cont1=0;
+    int cont2=0;
+    int cont3=0;
+    int cont4=0;
+    int cont5=0;
+    int cont6=0;
+    double A=0;
+    double B=0;
+      
 
-EthernetClient cliente = ArduinoTCP.available();
+    Serial.println("buf[0]");
+    Serial.println(buf[0]);  
+    buf[0]= cliente.read();
+    
+    //////////////////////////////////////////////
+    //        Leer el contenido del bufer       //
+    //////////////////////////////////////////////
+    
+    if (buf[7]=='T') {
+        //Keyboard.write(buf[6]);
+        Serial.println("Caracter");
+        Serial.println(buf[6]);
+    }
 
+    if  (buf[7]=='R') {
+        for (i=6; i>=0; i--) {
+            if (buf[i]==raton.x_plus[p]) {
+                cont1++;
+                if (cont1==7) {
+                    A= (510/sensitivity);
+                    B=0;
+                    Serial.println("A");
+                }
+            }
+            if (buf[i]==raton.x_less[p]) {
+                cont2++;;
+                if (cont2==7) {
+                    A= (510/sensitivity)*(-1);
+                    B=0;
+                    Serial.println("B");
+                }
+            }
+            if (buf[i]==raton.y_plus[p]) {
+                cont3++;
+                if (cont3==7) {
+                    A= 0;
+                    B= (510/sensitivity);
+                    Serial.println("C");
 
-char c;
+                }
+            }
+            if (buf[i]==raton.y_less[p]) {
+                cont4++;
+                if (cont4==7) {
+                    A= 0;
+                    B= (510/sensitivity)*(-1);
+                    Serial.println("D");
 
+                }
+            }
+            if (buf[i]==raton.ckd[p]) {
+                cont5++;
+                if (cont5==7) {
+                    //Mouse.press(MOUSE_RIGHT);  
+                    //delay(200);
+                    //Mouse.release(MOUSE_RIGHT);
+                    Serial.println("E");
+                }
+            }
+            if (buf[i]==raton.cki[p]) {
+                cont6++;
+                if (cont6==7) {
+                    //Mouse.press(MOUSE_LEFT);  
+                    //delay(200);
+                    //Mouse.release(MOUSE_LEFT);
+                    Serial.println("F");
 
-//c= cliente.read();
-//Serial.println(sizeof(char));
-/*
-if (c=='T') {
-
-   char cadena[2]={'0','0'};
-   cadena[0]=c;
-   cadena[1]= cliente.read();
-   
-   Serial.println("Contents:");
-   Serial.print("Primer elemento: ");
-   Serial.println(cadena[0]); //BIEN
-   Serial.print("Segundo elemento: ");
-   Serial.println(cadena[1]);
-
-  
-}
-
-if (c=='R') {
-
-char cadena[8]={'0','0','0','0','0','0','0','0'};
-cadena[0]=c;
-  Serial.println("Contents:");
-  Serial.println("Elemento 1: ");
-  Serial.println(cadena[0]); //BIEN
-
-int i;
-for (i=1; i<8; i++){
-  cadena[i]= cliente.read();
-  Serial.println("Elemento: ");
-  Serial.println(cadena[i], HEX);
-
-
-  }
-  
-}
-*/
-
-
-char prueba[7];
-//char c;
-
-int i;
-
-c= cliente.read();
-//char vect[1025];
-
-
-if(c=='T'||c=='R'){
-  
-Serial.println("///////////////");
-Serial.println(c);
-
-} else {
-
-Serial.println(c, HEX);
-//Serial.println(c);
-  
-}
-
-
-/*
-
-
-if (c=='T') {
-vect[cont]=c;
-Serial.print("///////////////");
-Serial.print("TIPO: ");
-Serial.println(vect[0]);
-Serial.print("BUFER: ");
-
-cont= cont + 1;
-
-}
-
-if (c=='R') {
-Serial.print("///////////////");
-char vect[8];
-vect[0]=c;
-Serial.print("TIPO: ");
-Serial.println(c);
-Serial.print("BUFER: ");
-}
-*/
-
-/*
-Serial.print("COMPROBACION: ");
-Serial.println(cont);
-Serial.print("C:");
-Serial.println(c);
-
-*/
-
-//Serial.print("C:");
-//Serial.println(c, HEX);
-
-/*
-if (c=='A') {
-  prueba[0]=c;
-Serial.print("Elemento: ");
-Serial.println(0);
-Serial.println(prueba[0]);
-for (i=1; i<7; i++){
-
-  prueba[i]= cliente.read();
- 
-  Serial.print("Elemento: ");
-  Serial.println(i);
-  Serial.println(prueba[i]);
-}
-  }
-  
-Serial.println("//////////////fin del bucle//////////////");
-
-prueba[0]= '0';
-prueba[1]= '0';
-prueba[2]= '0';
-prueba[3]= '0';
-prueba[4]= '0';
-prueba[5]= '0';
-prueba[6]= '0';
-Serial.println("//////////////datos borrados//////////////");
-
-
-
-
-*/
- /*
-if (cont==7) {
-
-  cont=0;
-  
-}
-*/
-
-/*
-
-Serial.print("Elemento: ");
-Serial.println(cont);
-Serial.println(prueba[cont]=cliente.read());
-
-*/
-
-/*
-
-char cadena[8]={'0','0','0','0','0','0','0','0'};
-
-int i=0;
-for (i=0; i<8; i++){
-  cadena[i]= cliente.read();
-  
-  }
-
-//c= cliente.read();
-
-   Serial.println("Contents:");
-   // Serial.println(c);
-   Serial.print("Primer elemento: ");
-   Serial.println(cadena[0]); //BIEN
-   Serial.print("Segundo elemento: ");
-   Serial.println(cadena[1], HEX);
-   Serial.print("Tercer elemento: ");
-   Serial.println(cadena[2], HEX);
-   Serial.print("Cuarto elemento: ");
-   Serial.println(cadena[3], HEX);
-   Serial.print("Quinto elemento: ");
-   Serial.println(cadena[4], HEX);
-   Serial.print("Sexto elemento: ");
-   Serial.println(cadena[5], HEX);
-   Serial.print("Séptimo elemento: ");
-   Serial.println(cadena[6], HEX);
-   Serial.print("Octavo elemento: ");
-   Serial.println(cadena[7], HEX);
-
-   */
-    delay (1000);
-    //exit(0);
+                }
+            }
+            p++;
+       }
+       //mouse.move(A,B,0);
+    }
+    
+    
+    //////////////////////////////////////////////
+    //              Rellenar el bufer           //
+    //////////////////////////////////////////////
+    
+    for (int i=7; i>0; i--) {
+      Serial.println("///////////////"); 
+      Serial.println(i); 
+      Serial.println(buf[i]);    
+      buf[i]= buf[(i-1)];      
+      delay(10);
+    }
+    
+      //Serial.println("buf[0]");
+      //Serial.println(buf[0]);  
+      //buf[0]= cliente.read();
+      
+    delay (5000);
 
 }
